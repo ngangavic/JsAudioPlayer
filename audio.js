13193a700a8bff880ab26c05b93de63695d8e2ff
ngangavic <ngangavictor10@gmail.com>
2019-11-15 19:39:36 +0300

    Audio JS

--- audio.js
+++ audio.js
@@ -1,46 +1,25 @@
-//js file
-class myAudioPlayer extends Audio {
-    constructor(element, options) {
-        super();
-        this.element = element;
-        
-        this.config = this.extend({
-            autoplay: false,
-            loop: false,
-            playlist: null
-        }, options);
+//play music function
+function play_music(src,icon) {
+    var myaudio = document.getElementById(src);
 
-        console.log(this.element);
-        console.log(typeof this.element[0].children);
+    if (!myaudio.paused) {
+        myaudio.pause();
+        $("#"+icon).removeClass('fa-pause-circle');
+        $("#"+icon).addClass('fa-play-circle');
+    } else {
+        myaudio.play();
+        $("#"+icon).removeClass('fa-play-circle');
+        $("#"+icon).addClass('fa-pause-circle');
     }
-    // function to convert seconds to hours minutes and seconds
-    secondsToTime = function(secs) {
-        let hours = Math.floor(secs / 3600),
-            minutes = Math.floor(secs % 3600 / 60),
-            seconds = Math.ceil(secs % 3600 % 60);
-        return (hours == 0 ? '' : hours > 0 && hours.toString().length < 2 ? '0' + hours + ':' : hours + ':') + (minutes.toString().length < 2 ? '0' + minutes : minutes) + ':' + (seconds.toString().length < 2 ? '0' + seconds : seconds);
-    }
+}
 
-    // function to check if the provided element is audio
-    checkIfAudioTag(element) {
-        return (element[0].tagName.toLowerCase() != 'audio') ? false : true;
-    }
+//pause music
+function pause_music(src) {
+    var myaudio = document.getElementById(src);
+    myaudio.pause();
+}
 
-    // extend function for extending the configs with user given
-    extend = function(defaults, options) {
-        var extended = {};
-        var prop;
-        for (prop in defaults) {
-            if (Object.prototype.hasOwnProperty.call(defaults, prop)) {
-                extended[prop] = defaults[prop];
-            }
-        }
-        for (prop in options) {
-            if (Object.prototype.hasOwnProperty.call(options, prop)) {
-                extended[prop] = options[prop];
-            }
-        }
-        return extended;
-    };
-
+//change icon colur
+function replace_class(icon) {
+    $("#"+icon).css("color","red");
 }
